78a227dcd9b09ac324d21e88c118c7c4aff23c6b
Adam LeCorney <adam.lecorney@funnel.io>
2024-01-24 09:06:26 +0100

    feat: outline for thing stack. no idea if or how it works

--- deploy/lib/deploy-stack.ts
+++ deploy/lib/deploy-stack.ts
@@ -1,16 +1,83 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as fs from "fs";
+import * as path from "path";
+import { Construct } from "constructs";
 
+// stolen from
+// https://github.com/aws/aws-cdk/issues/19303
+
+const iot = cdk.aws_iot;
 export class DeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // To create IotThing
+    const iotCoreThing = new iot.CfnThing(this, "iotCoreThing", {
+      thingName: "iotCoreThing-iot-thing",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // create certificate
+    const iotCoreThingCertificate2 = new iot.CfnCertificate(
+      this,
+      "IotCoreThingCertificate",
+      {
+        status: "ACTIVE",
+        certificateSigningRequest:
+          "arn:aws:acm-pca:::template/RootCACertificate/V1",
+        // caCertificatePem: fs
+        //   .readFileSync(`${__dirname}/../iot_certs/root-CA.crt`) // <---- this is th file I got from get started page.  i thought I can reuse it
+        //   .toString(),
+        // certificatePem: fs
+        //   .readFileSync(`${__dirname}/../iot_certs/raspi.cert.pem`) // <---- this is th file I got from get started page.  i thought I can reuse it
+        //   .toString(),
+      }
+    );
+
+    const iotCoreThingCertificate = new iot.CfnCertificate(
+      this,
+      "IotCoreThingCertificate",
+      {
+        status: "ACTIVE",
+        certificateSigningRequest: fs.readFileSync(
+          path.resolve("cert/cert.csr"),
+          "utf8"
+        ),
+      }
+    );
+
+    // create policy
+    const iotPolicy = new iot.CfnPolicy(this, "Policy", {
+      policyName: "Raspberry_Pi_Policy",
+      policyDocument: {
+        Version: "2012-10-17",
+        Statement: [
+          {
+            Effect: "Allow",
+            Action: ["iot:*"],
+            Resource: ["*"],
+          },
+        ],
+      },
+    });
+
+    // connect thing and certificate
+    const policyPrincipalAttachment = new iot.CfnPolicyPrincipalAttachment(
+      this,
+      "PolicyPrincipalAttachment",
+      {
+        policyName: iotPolicy.policyName || "PolicyPrincipalAttachment",
+        principal: iotCoreThingCertificate.attrArn,
+      }
+    );
+
+    // connect policy and certificate
+    const thingPrincipalAttachment = new iot.CfnThingPrincipalAttachment(
+      this,
+      "ThingPrincipalAttachment",
+      {
+        thingName: iotCoreThing.thingName || "ThingPrincipalAttachment",
+        principal: iotCoreThingCertificate.attrArn,
+      }
+    );
   }
 }
